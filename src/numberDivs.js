89e89999245086cf934668a65d83f3d3c5af47e2
IMH51 <ianmartinharrison@outlook.com>
2019-12-09 22:22:16 +0000

    imported handleClick, added onclick to new div

--- src/numberDivs.js
+++ src/numberDivs.js
@@ -1,8 +1,10 @@
 import { numberArray } from "./numberArray"
+import { handleClick } from "./colourFunctions"
 
 export const createNumberDiv = number => {
   let div = document.createElement("div")
   div.innerText = number
+  div.onclick = () => handleClick(number)
   return div
 }
 

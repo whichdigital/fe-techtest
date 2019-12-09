599f0f6ff1fe8eab61b3e3430567e40c3f1b5e46
IMH51 <ianmartinharrison@outlook.com>
2019-12-09 22:09:25 +0000

    added setDivColour and isMultiple helper function

--- src/colourFunctions.js
+++ src/colourFunctions.js
@@ -1,1 +1,7 @@
 let selectedNumber
+
+export const isMultiple = number => !(number % selectedNumber)
+
+export const setDivColour = div => {
+  div.style.backgroundColor = selectedNumber & isMultiple(div.innerText) ? "Yellow" : null
+}

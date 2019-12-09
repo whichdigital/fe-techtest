383b8a677f24c0f890f0c30b0bfd55f50956d76a
IMH51 <ianmartinharrison@outlook.com>
2019-12-09 22:24:48 +0000

    added missing & to ternary in setDivColour

--- src/colourFunctions.js
+++ src/colourFunctions.js
@@ -5,7 +5,7 @@ let selectedNumber
 export const isMultiple = number => !(number % selectedNumber)
 
 export const setDivColour = div => {
-  div.style.backgroundColor = selectedNumber & isMultiple(div.innerText) ? "Yellow" : null
+  div.style.backgroundColor = selectedNumber && isMultiple(div.innerText) ? "Yellow" : null
 }
 
 export const setAllDivColours = array => array.forEach(setDivColour)

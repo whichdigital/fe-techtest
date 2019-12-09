6e815fb61bd3ce3bf95b60f431a5e3a9091a4c82
IMH51 <ianmartinharrison@outlook.com>
2019-12-09 21:52:41 +0000

    wrote app function to add number divs to document

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import { allNumberDivs } from "./numberDivs"
 
 const app = () => {
-  
+  const numberContainer = document.createElement("div")
+  document.body.append(numberContainer)
+  numberContainer.append(...allNumberDivs)
 };
 
 export default app;

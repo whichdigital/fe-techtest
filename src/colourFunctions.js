1d6019351854e79db9cf0fc40f30a4e446e82932
IMH51 <ianmartinharrison@outlook.com>
2019-12-09 22:13:10 +0000

    added setAllDivColours function

--- src/colourFunctions.js
+++ src/colourFunctions.js
@@ -5,3 +5,5 @@ export const isMultiple = number => !(number % selectedNumber)
 export const setDivColour = div => {
   div.style.backgroundColor = selectedNumber & isMultiple(div.innerText) ? "Yellow" : null
 }
+
+export const setAllDivColours = array => array.forEach(setDivColour)

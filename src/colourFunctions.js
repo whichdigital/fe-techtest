930c5b8b6f4912108b63c32889f2fd743b59622c
IMH51 <ianmartinharrison@outlook.com>
2019-12-10 16:48:54 +0000

    added selected class and rewrote colourfunction

--- src/colourFunctions.js
+++ src/colourFunctions.js
@@ -5,7 +5,7 @@ let selectedNumber
 export const isMultiple = number => !(number % selectedNumber)
 
 export const setDivColour = div => {
-  div.style.backgroundColor = selectedNumber && isMultiple(div.innerText) ? "Yellow" : null
+  div.className = selectedNumber && isMultiple(div.innerText) ? "selected" : null
 }
 
 export const setAllDivColours = array => array.forEach(setDivColour)
